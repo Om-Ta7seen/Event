3429fef8803f97c5d75b7324a3e00e4ebda7663d
Zainab Hammami <zainab.hammami.zh@gmail.com>
2017-02-22 12:11:29 +0200

    user profile test

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,4 +1,9 @@
 var User = require('../model/user.js');
+var Event = require('../model/event.js');
+var UserAttendEvent = require('../model/userAttendEvent.js')
+var UserInterestEvent = require('../model/userInterestEvent.js')
+
+
 var Users = require('../collections/users');
 var util = require('../lib/utility.js');
 var jwt = require('jwt-simple');
@@ -16,50 +21,76 @@ module.exports = {
      } else {
        Users.create(user)
        .then(function(newUser) {
-        console.log("username in signup    ",newUser);
         var token = jwt.encode(newUser, 'not your bussines!!');
         newUser.set("password", "");
         newUser.token = token;
+        console.log(newUser.profile + "--------------------")
         res.json(newUser);
       });
      }
    });
   },
 
-  signin : function(req,res) {
-    var user = req.body
-    new User({ username: user.username }).fetch().then(function(found) {
-      if (found) {
-        var userHash = found.get('password');
-        util.comparePass(user.password, userHash, function(exist){
-          if(exist){
-            var token = jwt.encode(found, 'not your bussines!!');
-            found.set("password", "");
-            found.token = token;
-            res.json(found);
-          }else{
-            res.send("password is not correct");
-          }
-        })  
-      } else {
-        console.log("not found");
-        res.status(500).send("user not found");
-      }
-    });
-  },
+// console.log(found)
+
+signin : function(req,res) {
+  var user = req.body
+  new User({ username: user.username }).fetch().then(function(found) {
+    if (found) {
+      var userHash = found.get('password');
+      util.comparePass(user.password, userHash, function(exist){
+        if(exist){
+          var token = jwt.encode(found, 'not your bussines!!');
+          found.set("password", "");
+          found.token = token;
+          res.json(found);
+        }else{
+          res.send("password is not correct");
+        }
+      })  
+    } else {
+      console.log("not found");
+      res.status(500).send("user not found");
+    }
+  });
+},
 
 // Event.where({event}).fetch().then( function () {})
 
 getUserProfile: function (req, res) {
   var user = req.params.username;
+  var profile = {
+    goingEvents: [],
+    createdEvents: [],
+    interestedEvents: []
+  };
   // var user = req.params.username;
   
   User.where({username: user}).fetch().then( function (user) {
-    // user.set("password", "");
-    console.log(user)
     user.set('password', "")
-    var profile = {};
-    res.json(user)
+    profile.user = user
+    Event.where({userId: user.id}).fetch().then( function (events) {
+      for (var key in profile) {
+        if(key === 'createdEvents') {
+          profile['createdEvents'].push(events)
+        }
+      }
+    })
+    UserAttendEvent.where({userId: user.id}).fetch().then( function (events) {
+      for (var key in profile) {
+        if(key === 'attendEvents') {
+          profile['attendEvents'].push(events)
+        }
+      }
+    })
+    UserInterestEvent.where({userId: user.id}).fetch().then( function (events) {
+      for (var key in profile) {
+        if(key === 'interestEvents') {
+          profile['interestEvents'].push(events)
+        }
+      }
+    })
+    res.json(profile)
   })
 }
 };
