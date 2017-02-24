562199c8217e169d95779e208d3fbf35bc6aaf27
Zainab Hammami <zainab.hammami.zh@gmail.com>
2017-02-24 20:18:30 +0200

    upload

--- server/routes.js
+++ server/routes.js
@@ -3,21 +3,17 @@ var eventController = require('./controller/eventController.js');
 
 module.exports = function (app, express) {
 
+	app.post('/api/upload', userController.upload) 
+	app.get('api/topEventsByCity/:city', eventController.getTopCityEvents)
 
+	app.post('/api/events', eventController.addEvent);
+	app.get('/api/events', eventController.getAll);
+	app.put('/api/events/edit', eventController.editEvent)
+	app.delete('/api/events/delete', eventController.deleteEvent)
 
+	app.get('/api/events/:city', eventController.getAllCityEvents);
 
-app.get('api/topEventsByCity/:city', eventController.getTopCityEvents)
-
-app.post('/api/events', eventController.addEvent);
-app.get('/api/events', eventController.getAll);
-app.put('/api/events/edit', eventController.editEvent)
-app.delete('/api/events/delete', eventController.deleteEvent)
-app.post('/api/events/attending', userController.addAttending)
-app.post('/api/events/interested', userController.addInteresting)
-
-app.get('/api/events/:city', eventController.getAllCityEvents);
-
-app.post('/api/signup', userController.signup);
-app.post('/api/signin', userController.signin);
-app.get('/api/users/:username', userController.getUserProfile);
+	app.post('/api/signup', userController.signup);
+	app.post('/api/signin', userController.signin);
+	app.get('/api/users/:username', userController.getUserProfile);
 }
--- server/server.js
+++ server/server.js
@@ -1,16 +1,20 @@
 var express = require ('express');
 var partials = require('express-partials');
 var bodyParser = require('body-parser');
+var fileUpload = require('express-fileupload');
+// var multer  = require('multer')
+// var upload = multer({ dest: 'uploads/' })
+
 var app = express();
 
-
+app.use(fileUpload());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/../client'));
 
 require('./routes.js')(app , express);
 
-var port = 8000;
+var port = 8080;
 app.listen(process.env.PORT || port);
 
 console.log('Server now listening on port' + port);
