b146ecf4bf0655542860a6e5dc7f88af74c00018
montaserRahmani <montaser.rah@gmail.com>
2017-02-25 16:00:57 +0200

    adding Edit event, delete event done

--- server/server.js
+++ server/server.js
@@ -4,13 +4,16 @@ var bodyParser = require('body-parser');
 var fileUpload = require('express-fileupload');
 // var multer  = require('multer')
 // var upload = multer({ dest: 'uploads/' })
+var path = require('path')
 
 var app = express();
 
-app.use(fileUpload());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload());
 app.use(express.static(__dirname + '/../client'));
+
+app.use(bodyParser({keepExtensions:true,uploadDir:path.join(__dirname,'/files')}));
 
 require('./routes.js')(app , express);
 
